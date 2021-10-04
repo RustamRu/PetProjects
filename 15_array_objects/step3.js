cefb3e2df8c2e692e706df7b5d676c673ea8d838
RustamRu <kagapov.rustam@gmail.com>
2021-10-04 22:50:44 +0300

    15-04 Тег формы, отправка onsubmit

--- 15_array_objects/step3.js
+++ 15_array_objects/step3.js
@@ -1,16 +1,16 @@
-let someObject = {};
-const addItemBtn = document.getElementById("addItem");
+const someObject = {};
 const outputDiv = document.getElementById("output");
 
-addItemBtn.onclick = () => {
+addItem = () => {
     let key = document.getElementById("some-object-key").value;
     let value = document.getElementById("some-object-value").value;
     someObject[key] = value;
     console.log(someObject);
-    
+
     let output = '';
     for (key in someObject) {
-        output+=`<p>someObject[${key}] = ${someObject[key]}</p>`;
+        output += `<tr><td>${key}</td>
+        <td>${someObject[key]}</td></tr>`;
     }
     outputDiv.innerHTML = output;
 };
