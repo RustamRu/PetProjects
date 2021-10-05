3a52d272a933ef2f81a92f2ae7e57ce046d569dd
RustamRu <kagapov.rustam@gmail.com>
2021-10-05 18:48:24 +0300

    15-06 step3: DOMContentLoaded, preventDefault

--- 15_array_objects/step3.js
+++ 15_array_objects/step3.js
@@ -1,16 +1,21 @@
 const someObject = {};
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("addItemForm");
 
-addItem = () => {
-    const key = document.getElementById("some-object-key").value;
-    const value = document.getElementById("some-object-value").value;
-    const outputDiv = document.getElementById("output");
-    someObject[key] = value;
-    console.log(someObject);
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const key = document.getElementById("some-object-key").value;
+        const value = document.getElementById("some-object-value").value;
+        const outputDiv = document.getElementById("output");
 
-    let output = '';
-    for (const item in someObject) {
-        output += `<tr><td>${item}</td>
+        someObject[key] = value;
+        console.log(someObject);
+
+        let output = '';
+        for (const item in someObject) {
+            output += `<tr><td>${item}</td>
         <td>${someObject[item]}</td></tr>`;
-    }
-    outputDiv.innerHTML = output;
-};
+        }
+        outputDiv.innerHTML = output;
+    });
+});
